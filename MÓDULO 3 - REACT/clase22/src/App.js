01c4a29e0bc4d42af1ba31286e6b4de0646480c0
Celeste Dávila <celeedavila@gmail.com@>
2022-11-07 00:25:38 -0300

    primer clase react

---
+++ MÓDULO 3 - REACT/clase22/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Header from './components/layouts/Header';
+
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ MÓDULO 3 - REACT/clase22/src/components/layouts/Header.js
@@ -0,0 +1,13 @@
+const Header = (props) => {
+    return (
+        <header>
+            <p>hola soy el header</p>
+            <div>
+                <p>NO ME TIRA EL LOREMMMMMM
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
